af3a98a581b3bcc6b0e0ef7bb6025bc1d500f824
Giovanni Torelli <giovanni.walter@outlook.com>
2023-05-11 18:12:51 -0300

    Fixed 'deleteProject.js'

--- src/controllers/Projects/deleteProject.js
+++ src/controllers/Projects/deleteProject.js
@@ -1,6 +1,7 @@
 import { projects } from "./createProject";
 
 export default function deleteProject(project) {
-  const index = projects.indexOf(project);
+  const index = projects.findIndex((p) => p.title === project.title);
+
   projects.splice(index, 1);
 }
